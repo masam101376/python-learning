dec6775e267eeebde2fe9e48822d820fa6a9af56
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-13 00:08:59 +0900

    Learned about class & instance

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-print("Hello World!")
+# print("Hello World!")
+
+# 演算
+# print(1+1)
+
+# 変数
+# unko = 'Hello World2!'
+# print(unko)
+
+
+class Card:
+    def __init__(self, date, user_name):
+        self.date = date
+        self.user_name = user_name
+    def message(self):
+        return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
+
+date_a = '2020-01-01'
+user_name_a = 'Taro'
+card_a = Card(date_a, user_name_a)
+
+date_b = '2020-01-03'
+user_name_b = 'Hanako'
+card_b = Card(date_b, user_name_b)
+
+print(card_a.date, card_a.user_name, card_a.message())
+print(card_b.message())
