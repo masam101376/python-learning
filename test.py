391d6e851ba16903d8d1d8710e368bfe76300103
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-13 00:09:43 +0900

    Commented out codes on learning class & instance

--- test.py
+++ test.py
@@ -8,20 +8,21 @@
 # print(unko)
 
 
-class Card:
-    def __init__(self, date, user_name):
-        self.date = date
-        self.user_name = user_name
-    def message(self):
-        return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
+# class Card:
+#     def __init__(self, date, user_name):
+#         self.date = date
+#         self.user_name = user_name
+#     def message(self):
+#         return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
 
-date_a = '2020-01-01'
-user_name_a = 'Taro'
-card_a = Card(date_a, user_name_a)
+# date_a = '2020-01-01'
+# user_name_a = 'Taro'
+# card_a = Card(date_a, user_name_a)
 
-date_b = '2020-01-03'
-user_name_b = 'Hanako'
-card_b = Card(date_b, user_name_b)
+# date_b = '2020-01-03'
+# user_name_b = 'Hanako'
+# card_b = Card(date_b, user_name_b)
 
-print(card_a.date, card_a.user_name, card_a.message())
-print(card_b.message())
+# print(card_a.date, card_a.user_name, card_a.message())
+# print(card_b.message())
+
